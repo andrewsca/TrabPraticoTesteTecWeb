af43a44caf77f3f04b7f1de088aa60ccb7883332
Davi Silva <davia16@gmail.com>
2015-09-16 08:56:48 -0300

    adicionando o projeto de tecweb

---
+++ produto/codigo/cabecalho.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8"/>
+	<link rel="stylesheet" type="text/css" href="css/estilo.css">
+	<title><?php print $cabecalho_title; ?></title>
+</head>
+<body>
+	<header>
+		<h1>Economia Elétrica</h1>
+		
+	</header>
+	<nav>
+			<ul>
+				<li><a href="#">Pagina Inicial</a></li>
+				<li><a href="#">Cadastrar</a></li>
+				<li><a href="#">Lista de Produtos</a></li>
+				<li><a href="#">Relatórios</a></li>
+				<li><a href="#">Contato</a></li>
+				<li><a href="#">Ajuda</a></li>
+			</ul>
+	</nav>
+	
---
+++ produto/codigo/index.php
@@ -0,0 +1,28 @@
+<?php $cabecalho_title = "Principal"; require 'cabecalho.php';?>
+
+<div class="conteudo">
+
+	<h3>Cadastro</h3>
+	<form method="POST">
+		<label>Nome:</label>
+		<input type="text" name="username" id="username"></br>
+		<label>CPF:</label>
+		<input type="text" name="cpf" id="cpf"></br>
+		<label>E-mail:</label>
+		<input type="email" name="email" id="email"></br>
+		<label>Senha:</label>
+		<input type="password" name="senha" id="senha"></br>
+		<label>Endereço:</label>
+		<input type="text" name="endereco" id="endereco"></br>
+		<label>Data Nascimento:</label>
+		<input type="date" name="nascimento" id="nascimento"></br>
+		<input type="submit"  value="Cadastrar">
+	</form>
+		
+	<footer class="rodape">
+		<p>&copy; <?php print(date('Y')); ?> Todos os Direitos Reservados </p>
+	</footer>
+	</div>
+	
+</body>
+</html>
