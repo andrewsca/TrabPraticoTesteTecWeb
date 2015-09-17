5d086ed091fb5a47ffdad3d7862f14b45e8a3dd0
Frederico Drumond <fredrumond@gmail.com>
2015-09-17 13:52:05 -0300

    Tela Cadastro para Avaliação

--- produto/codigo/cadastro.php
+++ produto/codigo/cadastro.php
@@ -1,24 +1,33 @@
 <?php $cabecalho_title = "Cadastro"; require 'cabecalho.php';?>
 
 <div class="conteudo">
+	<div id="formulario-cadastro">		
+		<form class="formulario" method="POST">
+			<fieldset>
+				<legend>Cadastro no Sistema</legend>
+				<label>Nome:</label>
+				<input type="text" name="username" id="username" autofocus></br>
 
-	<h3>Cadastro</h3>
-	<form class="formulario-cadastro" method="POST">
-		<label>Nome:</label>
-		<input type="text" name="username" id="username"></br>
-		<label>CPF:</label>
-		<input type="text" name="cpf" id="cpf"></br>
-		<label>E-mail:</label>
-		<input type="email" name="email" id="email"></br>
-		<label>Senha:</label>
-		<input type="password" name="senha" id="senha"></br>
-		<label>Endereço:</label>
-		<input type="text" name="endereco" id="endereco"></br>
-		<label>Data Nascimento:</label>
-		<input type="date" name="nascimento" id="nascimento"></br>
-		<input type="submit"  value="Cadastrar">
-	</form>
-		
+				<label>CPF:</label>
+				<input type="text" name="cpf" id="cpf"></br>
+
+				<label>E-mail:</label>
+				<input type="email" name="email" id="email"></br>
+
+				<label>Senha:</label>
+				<input type="password" name="senha" id="senha"></br>
+
+				<label>Endereço:</label>
+				<input type="text" name="endereco" id="endereco"></br>
+
+				<label>Data Nascimento:</label>
+				<input type="date" name="nascimento" id="nascimento"></br>
+				<div id="botoes">					
+					<input type="submit"  value="Cadastrar">
+				</div>
+			</fieldset>
+		</form>
+	<div>	
 	<?php require 'rodape.php';?>
 	</div>
 	
