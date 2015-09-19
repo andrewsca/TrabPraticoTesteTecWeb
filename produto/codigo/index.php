23cc44204f9f90d59d9eb9ab9e228c56c3ffab7b
Davi Silva <davia16@gmail.com>
2015-09-19 00:26:12 -0300

    Adicionando conteúdo à pgina principal

--- produto/codigo/index.php
+++ produto/codigo/index.php
@@ -2,6 +2,22 @@
 
 <div class="conteudo">
 
+<p>Como já é sabido, vivemos em 2015 uma crise hídrica em nosso país, devido à escassez
+de chuvas nos últimos anos e à falta de planejamento de longo prazo das várias esferas
+de governo envolvidas. Neste cenário, políticas de multas e sobretaxas para desperdício
+de água e energia já foram implementadas, visto que nossa matriz energética se baseia
+fortemente em usinas hidrelétricas. As pessoas, portanto, estão se interessando
+crescentemente em descobrir maneiras de reduzir seus consumos de tais insumos e,
+assim, controlar seus gastos e minimizar o risco de agravar a crise.</p>
+<p>Como economizar Energia Elétrica
+
+Economizar energia elétrica é utilizá-la de forma a obter o máximo benefício com um menor consumo de energia, evitando os desperdícios ou o uso inadequado, sem no entanto, diminuir a qualidade, o conforto e a segurança.
+
+Por que Economizar ?
+
+Os custos crescentes, a escassez de recursos, a baixa remuneração, a disponibilidade de recursos hídricos, a otimização dos investimentos e os grandes desperdícios, fazem que seja importante a economia de energia elétrica para o nosso País e acarreta uma menor Nota Fiscal de Energia Elétrica a ser paga pelo consumidor. A economia de energia elétrica é uma fonte virtual de geração de energia elétrica, pois a energia deixa de ser desperdiçada. O custo do kWh economizado é cerca de 6 vezes mais barato do que o kWh gerado e ainda não agride ao meio ambiente.</p>
+	
+<h1><a href="http://www.light.com.br/para-residencias/Famlia-Consciente/dicas-de-economia.aspx">PODEMOS INCLUIR ALGO PARECIDO COM ISSO</a></h1>
 	<section id="login">
 		<form class="formulario-login">
 			<fieldset>
@@ -25,9 +41,9 @@
 		<fieldset>
 			<legend>Bandeiras Tarifarias</legend>
 			<ul>
-				<li>set/2015</li>
-				<li>ago/2015</li>
-				<li>jul/2015</li>
+				<li><a href="http://www.cemig.com.br/pt-br/atendimento/Paginas/Bandeiras_tarif%C3%A1rias.aspx">set/2015</a></li>
+				<li><a href="http://www.cemig.com.br/pt-br/atendimento/Paginas/Bandeiras_tarif%C3%A1rias.aspx">ago/2015</a> </li>
+				<li><a href="http://www.cemig.com.br/pt-br/atendimento/Paginas/Bandeiras_tarif%C3%A1rias.aspx">jul/2015</a></li>
 			</ul>
 
 		</fieldset>
